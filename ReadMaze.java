d4a64f4b946d8e3c2f061b621c8d100247a84a47
Jamie Zeng <jzeng3@stuy.edu>
2019-02-26 10:08:46 -0500

    ReadMaze works with char array

--- ReadMaze.java
+++ ReadMaze.java
@@ -11,6 +11,23 @@ public class ReadMaze{
         String line = in.nextLine();
         System.out.println(line);
       }
+
+      File maze_ = new File("Maze1.txt");
+      Scanner inf = new Scanner(maze_);
+      char[] array = new char[315];
+      while (inf.hasNextLine()){
+        String line = inf.nextLine();
+        // add the characters of each line to array
+        // print each element in the array
+        // print newline at the end of the line
+        for (int i = 0; i < line.length(); i++){
+          array[i] = line.charAt(i);
+          System.out.print(array[i]);
+          if (i == line.length() -1){
+            System.out.println();
+          }
+        }
+      }
     }catch(FileNotFoundException e){
       System.out.println("File not found");
     }
