5bc43500cf615347472a2c4df76eb9cae11407bb
Jamie Zeng <jzeng3@stuy.edu>
2019-02-28 20:45:57 -0500

    updated driver

--- Driver.java
+++ Driver.java
@@ -3,13 +3,19 @@ import java.io.*;
 
 public class Driver{
     public static void main(String[]args){
-      String filename = "Maze1.txt";
+      String filename = "";
+      if (args.length == 0){
+        filename = "data1.dat";
+      }
+      else{
+        filename = args[0];
+      }
+
       try{
         Maze f;
         f = new Maze(filename);//true animates the maze.
-
-      //  f.setAnimate(true);
-      //  f.solve();
+        f.setAnimate(true);
+        System.out.println("NUMBER OF STEPS: "+ f.solve());
         System.out.println(f);
       }catch(FileNotFoundException e){
         System.out.println("Invalid filename: "+filename);
